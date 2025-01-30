50a0f20b785e9bd58687ceeb63259de3d69c4f73
Voguh <voguhofc@protonmail.com>
2025-01-29 22:28:29 -0300

    feat: configure main start point

--- src/main/java/me/voguh/zomboid/authcompanion/Main.java
+++ src/main/java/me/voguh/zomboid/authcompanion/Main.java
@@ -1,13 +1,49 @@
 package me.voguh.zomboid.authcompanion;
 
+import me.voguh.zomboid.authcompanion.discord.DiscordManager;
+import me.voguh.zomboid.authcompanion.rcon.SourceRCONClient;
+import me.voguh.zomboid.authcompanion.settings.SettingsManager;
+import me.voguh.zomboid.authcompanion.util.EnvProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
+    private static SourceRCONClient rconClient;
+
     public static void main(String[] args) {
-        LOGGER.info("Hello, world!");
+        try {
+            EnvProperty.startupValidate();
+            SettingsManager.load();
+
+            String[] rconAddress = EnvProperty.RCON_ADDRESS.split(":");
+            if (rconAddress.length != 2) {
+                throw new IllegalArgumentException("Source RCON address must have <IP_ADDRESS>:<PORT>.");
+            }
+
+            rconClient = new SourceRCONClient(rconAddress[0], Integer.parseInt(rconAddress[1]), EnvProperty.RCON_PASSWORD);
+
+            DiscordManager.startClient(rconClient);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            System.exit(1);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    if (rconClient != null) {
+                        rconClient.close();
+                    }
+
+                    DiscordManager.stopClient();
+                } catch (Exception e) {
+                    LOGGER.error(e.getMessage(), e);
+                }
+            }
+        });
     }
 
 }
