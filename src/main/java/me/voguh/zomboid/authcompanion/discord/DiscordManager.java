12740b87f245f316b16089902efa82de3113fd94
Voguh <voguhofc@protonmail.com>
2025-01-29 22:28:16 -0300

    feat: implement discord integration with member change detections

---
+++ src/main/java/me/voguh/zomboid/authcompanion/discord/DiscordManager.java
@@ -0,0 +1,28 @@
+package me.voguh.zomboid.authcompanion.discord;
+
+import me.voguh.zomboid.authcompanion.discord.adapter.GuildMemberListenerAdapter;
+import me.voguh.zomboid.authcompanion.rcon.SourceRCONClient;
+import me.voguh.zomboid.authcompanion.util.EnvProperty;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.util.Set;
+
+public class DiscordManager {
+
+    private static JDA client;
+
+    public static void startClient(SourceRCONClient rconClient) {
+        Set<GatewayIntent> intents = Set.of(GatewayIntent.GUILD_MEMBERS);
+        client = JDABuilder.createLight(EnvProperty.DISCORD_TOKEN, intents)
+            .addEventListeners(new GuildMemberListenerAdapter(rconClient))
+            .build();
+    }
+
+    public static void stopClient() throws InterruptedException {
+        client.shutdown();
+        client.awaitShutdown();
+    }
+
+}
---
+++ src/main/java/me/voguh/zomboid/authcompanion/discord/adapter/GuildMemberListenerAdapter.java
@@ -0,0 +1,86 @@
+package me.voguh.zomboid.authcompanion.discord.adapter;
+
+import me.voguh.zomboid.authcompanion.rcon.SourceRCONClient;
+import me.voguh.zomboid.authcompanion.settings.SettingsManager;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberUpdateEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class GuildMemberListenerAdapter extends ListenerAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GuildMemberListenerAdapter.class);
+
+    private final SourceRCONClient rconClient;
+
+    public GuildMemberListenerAdapter(SourceRCONClient rconClient) {
+        this.rconClient = rconClient;
+    }
+
+    @Override
+    public void onGuildMemberRemove(@NotNull GuildMemberRemoveEvent event) {
+        String username = event.getUser().getAsTag().split("#")[0];
+
+        try {
+            rconClient.command("setaccesslevel " + username + " none");
+            rconClient.command("banuser " + username);
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        String username = event.getUser().getAsTag().split("#")[0];
+        List<Role> roles = event.getMember().getRoles();
+
+        try {
+            if (isPlayer(roles)) {
+                rconClient.command("unbanuser \"" + username + "\"");
+
+                String accessLevel = isAdmin(roles) ? "admin" : isModerator(roles) ? "moderator" : "none";
+                rconClient.command("setaccesslevel \"" + username + "\" \"" + accessLevel + "\"");
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void onGuildMemberUpdate(@NotNull GuildMemberUpdateEvent event) {
+        String username = event.getUser().getAsTag().split("#")[0];
+        List<Role> roles = event.getMember().getRoles();
+
+        try {
+            if (isPlayer(roles)) {
+                rconClient.command("unbanuser \"" + username + "\"");
+
+                String accessLevel = isAdmin(roles) ? "admin" : isModerator(roles) ? "moderator" : "none";
+                rconClient.command("setaccesslevel \"" + username + "\" \"" + accessLevel + "\"");
+            } else {
+                rconClient.command("setaccesslevel " + username + " none");
+                rconClient.command("banuser " + username);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    private boolean isAdmin(List<Role> roles) {
+        return roles.stream().anyMatch(role -> role.getId().equals(SettingsManager.settings().getAdminsRoleId()));
+    }
+
+    private boolean isModerator(List<Role> roles) {
+        return roles.stream().anyMatch(role -> role.getId().equals(SettingsManager.settings().getModeratorsRoleId()));
+    }
+
+    private boolean isPlayer(List<Role> roles) {
+        return roles.stream().anyMatch(role -> role.getId().equals(SettingsManager.settings().getPlayersRoleId()));
+    }
+
+}
