808e7269cb3ee12e9df255d1df99f869781dee73
Voguh <voguhofc@protonmail.com>
2025-02-02 16:00:09 -0300

    feat: add env property detection for config.ini

--- src/main/java/me/voguh/zomboid/authcompanion/util/EnvProperty.java
+++ src/main/java/me/voguh/zomboid/authcompanion/util/EnvProperty.java
@@ -5,8 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EnvProperty {
+
+    private static final Map<String, String> CONFIG_FILE = readConfig();
 
     @Required
     public static final String DISCORD_TOKEN = getEnvOrProperty("DISCORD_TOKEN");
@@ -34,8 +43,8 @@ public class EnvProperty {
 
     public static void startupValidate() throws Exception {
         for (Field field : EnvProperty.class.getDeclaredFields()) {
-            if (field.isAnnotationPresent(Required.class) && Strings.isNullOrEmpty((String) field.get(null))) {
-                throw new Exception("Missing or empty required property '" + field.getName() + "'");
+            if (Modifier.isPublic(field.getModifiers()) && field.isAnnotationPresent(Required.class) && Strings.isNullOrEmpty((String) field.get(null))) {
+                throw new Exception("Required property '" + field.getName() + "' can't be founded in environment variables, java properties or config.ini file.");
             }
         }
     }
@@ -46,7 +55,34 @@ public class EnvProperty {
             return env;
         }
 
-        return System.getProperty(name);
+        String prop = System.getProperty(name);
+        if (prop != null) {
+            return prop;
+        }
+
+        return CONFIG_FILE.get(name);
+    }
+
+    private static Map<String, String> readConfig() {
+        try {
+            Map<String, String> result = new HashMap<>();
+            List<String> lines = Files.readAllLines(Path.of("config.ini"), StandardCharsets.UTF_8);
+
+            for (String line : lines) {
+                line = line.split("#")[0];
+
+                if (!Strings.isNullOrEmpty(line)) {
+                    String[] entry = line.split("=", 2);
+                    String key = entry[0].trim();
+                    String value = (entry.length > 1 ? entry[1] : "").trim();
+                    result.put(key, value);
+                }
+            }
+
+            return result;
+        } catch (Exception e) {
+            return new HashMap<>();
+        }
     }
 
 }
